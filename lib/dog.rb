bd0adce753438d0053ac671492c200d8e1dbd580
rtyenriques <tooshopboys@gmail.com>
2021-01-26 11:26:43 -0800

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,9 @@
 class Dog
-    
-def name=(name)
-    @name = name
+
+attr_accessor :name, :breed
+
+def breed= (breed)
+  @breed = breed
 end
 
-def name
-    @name
-end
-
-def breed=(breed)
-    @breed = breed
-end
-
-def breed 
-    @breed
-end
 end
